c98a3d17454d6cf348457e23a308131151a10e07
Rajan <rajan121.vishal121@gmail.com>
2022-01-11 01:24:44 +0530

    Final commit

---
+++ opentoright/src/Accordian.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function Accordian({ text, index, children, isExpand = false }) {
+    const [expand, setExpand] = useState(isExpand)
+
+
+    return (
+        <div className="container">
+            <div className="header" onClick={() => setExpand(!expand)}>
+
+                <h1 className="title">{index}.   {text} {index}</h1>
+                {!expand && <span><i class="fa fa-angle-right" ></i></span>}
+                {expand && <span><i class="fa fa-angle-left"></i></span>}
+                
+
+            </div>
+
+
+
+            {expand && <div className="content">
+                {children}
+            </div>}
+            
+        </div>)
+}
+
+export default Accordian;
