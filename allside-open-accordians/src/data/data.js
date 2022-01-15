dba7979f5c1501866739829046aacdc36583d92a
Rajan <rajan121.vishal121@gmail.com>
2022-01-15 17:33:03 +0530

    final

---
+++ allside-open-accordians/src/data/data.js
@@ -0,0 +1,16 @@
+export  const data=[
+    {
+        que:"How are yoy?",
+        ans:"Chaka Cha.k"
+    },
+    {
+        que:"what are u doing these days ?",
+        ans:"I am doing trainning from 10x Academy of Full-stack-development."
+
+    },
+    {
+        que:"What is carrer possiblities is ur bootcamp ?",
+        ans:"Carrer possiblities is really good in my bootcamp looking forward to sit in placements."
+    }
+
+]
